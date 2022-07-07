51b409b92caeea6ce31d6d9b95b585177facf38a
sgiandomenico <sgiandomenico@users.noreply.github.com>
2022-07-07 13:57:15 -0700

    Add a JavaScript sample file.

---
+++ samples/javascript.js
@@ -0,0 +1,23 @@
+const constKey = 'const';
+let letKey = 'let';
+
+function f(x) { return x; }
+
+const OBJ = {
+  simpleProp: 0,
+  'singleQuotedProp': 0,
+  "doubleQuotedProp": 0,
+  ['bracketedSingleQuotedProp']: 0,
+  ["bracketedDoubleQuotedProp"]: 0,
+  [constKey]: 0,
+  [letKey]: 0,
+  [letKey > 0 ? 'gt' : 'lt']: 0,
+  ['ab' + 'cd']: 0,
+  [f(0)]: 0,
+  [f('foo')]: 0,
+  method() { },
+  get computedProp() { },
+  set computedProp(value) { },
+  [Symbol.toPrimitive]: 0,
+  [Symbol.iterator]() { },
+};
